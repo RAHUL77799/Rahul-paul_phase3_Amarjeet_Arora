d059e9b050e14bbcf6ea4df75ded9199dddc0ba9
Rahul <Rahulpaulrock800@gmail.com>
2021-05-17 23:54:52 -0700

    Initial commit

---
+++ main/java/com/example/sportyshoes1/ecommerce/controller/Usercontroller.java
@@ -0,0 +1,31 @@
+package com.example.sportyshoes1.ecommerce.controller;
+
+
+
+import com.sportyshoes.ecommerce.model.User;
+import com.sportyshoes.ecommerce.repository.UserRepository;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(value = { "", "/" })
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping(value = { "", "/" })
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
---
+++ main/java/com/example/sportyshoes1/springcontextTest.java
@@ -0,0 +1,19 @@
+package com.example.sportyshoes1;
+
+package com.example.sportyshoes1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sportyshoes.ecommerce.EcommerceApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EcommerceApplication.class)
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
